e77a9c779c9738dd95f26a8238efc24e6257acaa
laurencetroyv-Hailan <laurencetroyv@gmail.com>
2021-11-14 21:12:37 +0800

    Adding Binary Search Tree and its Node

---
+++ src/BinaryTree/BSTNode.java
@@ -0,0 +1,21 @@
+package BinaryTree;
+
+/************************* BSTNode.java ************************** node of a generic binary search tree */
+public class BSTNode {
+    protected Comparable el;
+    protected BSTNode left, right;
+
+    public BSTNode() {
+        left = right = null;
+    }
+
+    public BSTNode(Comparable el) {
+        this(el, null, null);
+    }
+
+    public BSTNode(Comparable el, BSTNode lt, BSTNode rt) {
+        this.el = el;
+        left = lt;
+        right = rt;
+    }
+}
